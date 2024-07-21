acfb56f5690113a6d495feedb9cdbb6329aa0b84
Jeremy <jeremypatamia02@gmail.com>
2024-07-21 05:38:40 +0000

    Add a footer

--- src/app/ui/homepage/features.js
+++ src/app/ui/homepage/features.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import Featurecard from "./featurecard";
+import FeatureCard from "./featurecard";
 import featureData from "./data";
+
 export default function Features() {
   return (
-    <section className="mt-[80px]">
+    <section className="mt-20">
       <div className="flex flex-col justify-center items-center mb-4">
-        <h1 className="text-6xl mb-4">Features</h1>
+        <h1 className="text-4xl md:text-5xl lg:text-6xl mb-4">Features</h1>
         <Image
           src="/images/about-underline.png"
           alt="About Us underlined"
@@ -13,19 +14,15 @@ export default function Features() {
           height={12}
         />
       </div>
-      {/*Card Area*/}
-
-      <div className="flex justify-center gap-4 mt-[80px]">
+      {/* Card Area */}
+      <div className="flex flex-wrap justify-center gap-4 mt-20">
         {featureData.map((feature) => (
-         
-          <Featurecard
+          <FeatureCard
             key={feature.img}
             photo={feature.img}
             title={feature.title}
             info={feature.info}
-            
           />
-        
         ))}
       </div>
     </section>
